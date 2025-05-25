8ae50fe98b58bd72d427252e8e6e8cc8e159078c
Samson96h <samsonhovhannisyan96@gmail.com>
2025-05-25 18:13:52 +0400

    new game update

--- millionaire.py
+++ millionaire.py
@@ -1,74 +1,139 @@
-import random
+from random import randint, shuffle
 
-def get_quest(fname):
-    f = open(fname)
-    lines = f.readlines()
+QUESTIONS_COUNT = 10
+
+def player_or_admin ():
+    answ = input("Want to start a game or add new questions? play or edit : ")
+    if answ == "play":
+        main()
+    if answ == "edit":
+        login = input("Enter password : ")
+        if login == "admin":
+            edit()
+        else:
+            print('wrong password')
+
+def edit():
+    new_quest = input("Please type the new question: ").strip()
+
+    if not new_quest.endswith("?"):
+        new_quest += "?"
+
+    answer_input = input("Enter 4 answers (first one is correct), separated by commas: ").strip()
+    answers = [a.strip() for a in answer_input.split(",")]
+
+    if len(answers) != 4:
+        print("You must enter exactly 4 answers (1 correct + 3 incorrect).")
+        return
+    
+    question_line = new_quest + "," + ",".join(answers)
+
+    f = open("questions.txt", "a")
+    f.write(question_line + "\n")
     f.close()
-    return lines
+    
+    print("Your question has been added successfully.")
 
-quest_list = get_quest("questions.txt")
 
-def questions_craft(list):
-    md_quest = []
-    for i in list:
-        i = i.strip()
-        ind = i.index("?")
-        question = i[:ind + 1]
-        answers = [a.strip() for a in i[ind + 1:].split(",")]
-        md_quest.append({
-            "question": question,
-            "answers": answers,
-            "correct": answers[0]
-        })
-    return random.sample(md_quest, 10)
 
-def run_quiz():
-    player_name = input("Please enter your name : ")
-    quests = questions_craft(get_quest("questions.txt"))
+def get_questions(fname):
+    with open(fname) as f:
+        return [line.strip() for line in f.readlines() if "?" in line]
+
+def get_random_indexes(qcount, count):
+    indexes = []
+    while len(indexes) < count:
+        ind = randint(0, qcount - 1)
+        if ind not in indexes:
+            indexes.append(ind)
+    return indexes
+
+def prepare_questions(questions):
+    final = []
+    for q in questions:
+        if "?" in q:
+            q_text, ans_text = q.split("?", 1)
+            q_text += "?"
+            answers = [a.strip() for a in ans_text.split(",")]
+            correct = answers[0]
+            shuffle(answers)
+            final.append({
+                "question": q_text,
+                "variants": answers,
+                "correct": correct
+            })
+    return final
+
+def play_quiz(questions):
     score = 0
-    for q in quests:
+    for q in questions:
         print("\n" + q["question"])
-        random.shuffle(q["answers"])
-        for i, answer in enumerate(q["answers"], 1):
-            print(f"{i}. {answer}")
+        for i, a in enumerate(q["variants"], 1):
+            print(f"{i}. {a}")
 
-        user = int(input("Your answer please (1 - 4): "))
-        user = q["answers"][user - 1]
-        if user == q["correct"]:
-            print("YESSS !")
+        choice = input("Your answer (1-4): ")
+
+        if choice.isdigit():
+            num = int(choice)
+            if 1 <= num <= 4:
+                answer = q["variants"][num - 1]
+            else:
+                print("Invalid number! Must be between 1 and 4.")
+                answer = ""
+        else:
+            print("Invalid input! Please enter a number.")
+            answer = ""
+
+        if answer == q["correct"]:
+            print("Correct!")
             score += 1
         else:
-            print(f"NOOO ! {q['correct']}")
+            if answer != "":
+                print(f"Wrong! Correct answer was: {q['correct']}")
+    
+    return score
 
-    print(f"Your score : {score} / 10")
 
-    with open("top.txt", "a") as f:
-        f.write(player_name + ' : ' + str(score) + "\n")
+def save_score(fname, username, score):
+    with open(fname, "a") as f:
+        f.write(f"{username} : {score}\n")
 
-    with open("top.txt", "r") as f:
-        results = f.readlines()
+def load_scores(fname):
     scores = []
-    for line in results:
-        line = line.strip()
-        if " : " in line:
-            info = line.split(" : ")
-            if len(info) == 2 and info[1].isdigit():
-                name = info[0]
-                score = int(info[1])
-                scores.append((name, score))
+    with open(fname, "r") as f:
+        for line in f:
+            if " : " in line:
+                name, scr = line.strip().split(" : ")
+                if scr.isdigit():
+                    scores.append((name, int(scr)))
+    return scores
 
+def save_sorted_scores(fname, scores):
     scores.sort(key=lambda x: x[1], reverse=True)
-    with open("top.txt", "w") as f:
+    with open(fname, "w") as f:
         for name, score in scores:
             f.write(f"{name} : {score}\n")
 
-    def top_players():
-        print("\n TOP PLAYERS ")
-        for i, (name, score) in enumerate(scores[:10], 1):
-            print(f"{i}. {name} - {score}")
+def show_top(scores, top_n=10):
+    print("\nTOP PLAYERS:")
+    for i, (name, score) in enumerate(scores[:top_n], 1):
+        print(f"{i}. {name} - {score}")
 
-    answer = input("Do you want to see the best players? y / n: ")
-    if answer.lower() == "y":
-        top_players()
+def main():
+    username = input("Enter your username: ")
+    all_questions = get_questions("questions.txt")
+    indexes = get_random_indexes(len(all_questions), QUESTIONS_COUNT)
+    selected = [all_questions[i] for i in indexes]
+    prepared = prepare_questions(selected)
+    score = play_quiz(prepared)
+    print(f"\nYour final score: {score}/{QUESTIONS_COUNT}")
+    
+    save_score("top.txt", username, score)
+    scores = load_scores("top.txt")
+    save_sorted_scores("top.txt", scores)
 
-run_quiz()
+    show = input("Do you want to see the top players? (y/n): ")
+    if show.lower() == "y":
+        show_top(scores)
+
+player_or_admin()
